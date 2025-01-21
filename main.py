248cede69ab27eb76991d5ef9e9b4a1ba4582422
RajivC92 <rajcv92@gmail.com>
2025-01-21 16:26:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 def function_cicd(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-    """
-    request_json = request.get_json()
+    """Responds to HTTP request."""
+    # Check if JSON payload exists
+    request_json = request.get_json(silent=True)
     if request.args and 'message' in request.args:
         return request.args.get('message')
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return f'Function - 1 with V1.0 with CI/CD Pipeline'
+        return 'Function - 1 with V1.0 with CI/CD Pipeline'
